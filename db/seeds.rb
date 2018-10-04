1dc5079d247bce487a39ee68d2c939225d9cea08
Lisa Caton <lisacaton01@gmail.com>
2018-10-04 12:48:38 -0400

    seeding data complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,25 @@
 user = User.create!(
   email: 'Apple@gmail.com',
   password: 'ApplePear'
-) 
-Item.create!(
-  user: user,
-  name: 'Apple'
 )
+20.times do
+  Item.create!(
+    user: user,
+    name: Faker::FamilyGuy.quote
+  )
+end
 puts "Seed Finished"
 puts"#{User.count} users created"
 puts "#{Item.count} items created"
+
+# user = User.create!(
+#   email: 'Apple@gmail.com',
+#   password: 'ApplePear'
+# )
+# Item.create!(
+#   user: user,
+#   name: 'Apple'
+# )
+# puts "Seed Finished"
+# puts"#{User.count} users created"
+# puts "#{Item.count} items created"
