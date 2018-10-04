70c26081c984bc8e61b0862bc2356ab47d287377
Lisa Caton <lisacaton01@gmail.com>
2018-10-04 12:27:20 -0400

    seed data working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create([email: 'tester1@test.com'])
-users = User.all
-Item.create([{
-  user: users.sample,
-  name: 'Steve'
-   }])
+user = User.create!(
+  email: 'Apple@gmail.com',
+  password: 'ApplePear'
+) 
+Item.create!(
+  user: user,
+  name: 'Apple'
+)
 puts "Seed Finished"
 puts"#{User.count} users created"
-puts "#{Item.count} items created" 
+puts "#{Item.count} items created"
